4ada9fc52240a1e93fa298d55b12fd04fe909b50
Hemanth <Hemanth>
2020-12-13 18:00:05 +0530

    GCP Deplomnet Commit

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ def index():
 
 if __name__ == "__main__":
     #app.run(host='127.0.0.1', port=8001, debug=True)
-	application.run(debug=True) # running the app
+    app.run(debug=True) # running the app
